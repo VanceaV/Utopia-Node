53f3fba8661a4bcf94d53c33291b22c139459672
VanceaV <vancea.vladimir@gmail.com>
2019-05-01 17:52:22 -0400

    Second Commit

--- controller/TravelerController.js
+++ controller/TravelerController.js
@@ -1,6 +1,6 @@
 var routes = require('express').Router();
-var db = require('../dao/Db');
-var airPortDao = require('../dao/AirPortDao');
+var airPortDao = require('../dao/AirportDao');
+var flightDao = require('../dao/FlightDao');
 
 
 routes.get('/airports/:airport_name',function(req,res){
@@ -9,6 +9,30 @@ routes.get('/airports/:airport_name',function(req,res){
       res.setHeader('Content-Type', 'application/json');
       res.send(result);
     });
+});
+
+
+routes.get('/flights/:date/:depAirportId/:arrAirportId',function(req,res){
+  flightDao.getFlightList(req.params.date, req.params.depAirportId, req.params.arrAirportId, function(error, result){
+    if(error) throw error;
+    res.setHeader('Content-Type', 'application/json');
+    res.send(result);
+  });
+});
+
+
+routes.put('/flights', function(req, res){
+  var data = req.body;
+  flightDao.decreaseFlightCapacity(data, function(err, result){
+    if(err){
+      res.status(400);
+      res.send('Update flights Failed!');
+    }else{
+      res.status(201);
+    res.send('Update Author Successful!');
+    }
+  });
+
 });
 
 module.exports = routes;
--- dao/FlightDao.js
+++ dao/FlightDao.js
@@ -1,24 +1,62 @@
 var db = require('./Db');
 var mysql = require('mysql');
 
-exports.getFlightList = function(date,depAirportId,arrAirportId, cb){
+const adminId = 1;
 
-    var query = "SELECT * FROM city inner join airport using(city_id)" + 
-        "where dep_airport_id ="+ mysql.escape(depAirportId)+
-        "and arr_airport_id ="+ mysql.escape(arrAirportId)+
-        "or country="+mysql.escape(airport_name)+
-        "or airport_code="+mysql.escape(airport_name);
-        
-    db.query(query, function(err, result,fields){
-        cb(err, result,fields);
-      });
+const today = new Date();
 
+console.log(today);
+
+
+exports.getFlightList = function (date, depAirportId, arrAirportId, cb) {
+
+    var query = "SELECT * From flight " +
+        "where dep_airport_id =" + mysql.escape(depAirportId) +
+        "and arr_airport_id =" + mysql.escape(arrAirportId) +
+        "and dep_dateTime >=" + mysql.escape(date) +
+        "and dep_dateTime <DATE_ADD(" + mysql.escape(date) + ", INTERVAL 1 DAY)" +
+        "order by dep_dateTime";
+
+    db.query(query, function (err, result, fields) {
+        cb(err, result, fields);
+    });
 }
 
-value = "SELECT * From flight " + 
-			"where dep_airport_id =:depAirportId and arr_airport_id =:arrAirportId " + 
-			"and dep_dateTime >=:date and dep_dateTime <DATE_ADD(:date, INTERVAL 1 DAY) "
-			+ "order by dep_dateTime", nativeQuery = true
+
+exports.decreaseFlightCapacity = function (data, cb) {
+
+    console.log(data);
+
+    var flightsArray = data.flights;
+    db.beginTransaction(function (err) {
+        if (err) cb(err, null);
+
+        for (var i = 0; i < flightsArray.length; i++) {
+
+            console.log(flightsArray[i].flightId);
+
+            var query = "update flight set capacity = capacity - " + mysql.escape(data.travelerNumber) +
+                ", update_by = " + mysql.escape(adminId) +
+                ", update_date = " + mysql.escape(today) +
+                " where flight_id = " + mysql.escape(flightsArray[i].flightId);
+            db.query(query, function (err, result, fields) {
+                if(err) {
+                    db.rollback(function(){
+                        console.log(err);
+                        throw err;
+                    })
+                }
+                //cb(err, result,fields);
+            });
+
+        }
+
+
+        db.commit(function(err, res){
+          cb(err, res);
+        }); 
+    });
+};
 
 
 
@@ -26,8 +64,4 @@ value = "SELECT * From flight " +
 
 
 
-@Query(value = "SELECT * From flight " + 
-"where dep_airport_id =:depAirportId and arr_airport_id =:arrAirportId " + 
-"and dep_dateTime >=:date and dep_dateTime <DATE_ADD(:date, INTERVAL 1 DAY) "
-+ "order by dep_dateTime", nativeQuery = true)
-List<Flight> getFlightList(@Param("date") Date date, @Param("depAirportId") Long depAirportId, @Param("arrAirportId") Long arrAirportId);
+
